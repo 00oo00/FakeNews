939e4e2cb90333dc0571c1d8b76a1845add2627a
Unknown <david@jonsson.vg>
2017-03-20 14:40:21 +0100

    mera randomize

--- www/fn/js/main.js
+++ www/fn/js/main.js
@@ -1,25 +1,34 @@
 var arrPuff = [];
+var arrArticles = [];
 
-$.getJSON("json/articles.json", function(data){
+
+$.getJSON("json/articles.json", function (data) {
 	console.log(data);
 	$(start);
-	arrPuff = data;
+	arrArticles = data;
 });
 
-function radomPuff(){
-	var randomized = Math.random(0, arrPuff.length);
-	console.log(randomized);
-	return randomized;
-	
+
+function randomPuff() {
+	arrPuff.splice(0,arrPuff.length);
+	var randomized;
+	for (let i = 0; i < 1; i++) {
+		
+		randomized = Math.floor(Math.random() * arrArticles.length) + 0;
+		arrPuff.forEach(function(puff){
+			if(puff.id == iu7)
+		});
+		
+		arrPuff.push(arrArticles[randomized]);
+	}
 }
 
+	function start() {
+		
+		
+		$('body').append(startpage());
+		$('#page-content').append(article());
+		$('#page-content').append(randomPuff());
+	}
 
-function start(){
-	radomPuff();
-	$('body').append(startpage());
-  	$('#page-content').append(article());
-	$('#page-content').append(puff(arrPuff[0]));
-	$('#page-content').append(puff(arrPuff[1]));
-	$('#page-content').append(puff(arrPuff[2]));
-	
-}
+
