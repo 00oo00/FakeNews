bec8b820bfcf44c75b1b65ecc0ea62c69b3a7c08
Unknown <david@jonsson.vg>
2017-03-20 11:48:51 +0100

    Kopplat puffar till resten

--- www/fn/js/main.js
+++ www/fn/js/main.js
@@ -1,8 +1,20 @@
-$(start);
+var arrPuff = [];
+
+$.getJSON("json/articles.json", function(data){
+	console.log(data);
+	$(start);
+	arrPuff = data;
+});
+
+
+
 
 function start(){
 	
 	$('body').append(startpage());
 
   $('#page-content').append(article());
+	$('#page-content').append(puff(arrPuff[0]));
+	$('#page-content').append(puff(arrPuff[1]));
+	
 }
