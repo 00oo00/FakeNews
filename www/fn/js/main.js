1f5cea982f395bb9402f32fe7a7eda76ba1d6976
Ramona Chantaf <ramonach.91@gmail.com>
2017-03-20 13:41:31 +0100

    lagt in puffar i article template

--- www/fn/js/main.js
+++ www/fn/js/main.js
@@ -1,10 +1,19 @@
 $(start);
 
+var puffs = [];
+
 function start(){
-	
+
+	$.getJSON('json/articles.json', function(data){
+		puffs.push(data[1]);
+		loadDom();
+	});
+
+
+} 
+
+function loadDom(){
 	$('body').append(startpage());
-
   $('#page-content').append(article());
-  	$('#page-content').append(puff());
-
+  	//$('#page-content').append(puff());
 }
