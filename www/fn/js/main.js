5986197dd39021d3bcd93093cc7a1483434207ac
Unknown <david@jonsson.vg>
2017-03-20 13:27:40 +0100

    .

--- www/fn/js/main.js
+++ www/fn/js/main.js
@@ -6,15 +6,21 @@ $.getJSON("json/articles.json", function(data){
 	arrPuff = data;
 });
 
-
+function radomPuff(){
+	var randomized = Math.random(0, attPuff.length);
+	console.log(randomized);
+	return randomized;
+	
+}
 
 
 function start(){
 	
 	$('body').append(startpage());
-
-  $('#page-content').append(article());
+	$('body').randomPuff();
+  	$('#page-content').append(article());
 	$('#page-content').append(puff(arrPuff[0]));
 	$('#page-content').append(puff(arrPuff[1]));
+	$('#page-content').append(puff(arrPuff[2]));
 	
 }
